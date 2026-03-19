e3311e80064cf46b77d91a95074e5be0fa296750
Tomasz Marek <tomasz.marek02@gmail.com>
2026-03-19 01:54:32 +0100

    Add user-service: Maven POM, User model, UserRepository, DTOs

---
+++ user-service/src/main/java/com/medicalapp/userservice/model/User.java
@@ -0,0 +1,40 @@
+package com.medicalapp.userservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    public enum Role {
+        PATIENT, DOCTOR, ADMIN
+    }
+}
